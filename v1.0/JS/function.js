db4c6351cfe51f9d3a92c44fc3f15caf7d612735
alexkjzz <Alex.lam@orange.fr>
2024-04-09 11:26:26 +0200

    javaupdate

--- v1.0/JS/function.js
+++ v1.0/JS/function.js
@@ -1,53 +1,8 @@
 
+var jsdata = JSON.parse('{{ json_data | safe }}');
+console.log(jsdata)
 
-var k = "" //variable du nombre de favoris qui sera determiné par l'id "i" des films..
-const films = [
-    {
-        id: 1,
-        titre: "Star Wars: Episode I",
-        statistique: "Nombre de favoris: " + k,
-    },
-    {
-        id: 2,
-        titre: "Star Wars: Episode II",
-        statistique: "Nombre de favoris: " + k,
-    },
-    {
-        id: 3,
-        titre: "Star Wars: Episode III",
-        statistique: "Nombre de favoris: " + k,
-    },
-    {
-        id: 4,
-        titre: "Star Wars: Episode IV",
-        statistique: "Nombre de favoris: " + k,
-    },
-    {
-        id: 5,
-        titre: "Star Wars: Episode V",
-        statistique: "Nombre de favoris: " + k,
-    },
-    {
-        id: 6,
-        titre: "Star Wars: Episode VI",
-        statistique: "Nombre de favoris: " + k,
-    },
-    {
-        id: 7,
-        titre: "Star Wars: Episode VII",
-        statistique: "Nombre de favoris: " + k,
-    },
-    {
-        id: 8,
-        titre: "Star Wars: Episode VIII",
-        statistique: "Nombre de favoris: " + k,
-    },
-    {
-        id: 9,
-        titre: "Star Wars: Episode IX",
-        statistique: "Nombre de favoris: " + k,
-    }
-];
+
 
 function creerListeFilms(){
 
@@ -58,15 +13,14 @@ function creerListeFilms(){
         carteFilm.classList.add("carte-film")
         carteFilm.id = "film-"+ film.id;
 
-        const titre = document.createElement("h2");
-        titre.textContent = film.titre;
+        const title = document.createElement("h2");
+        title.textContent = film.title;
 
         const statistique = document.createElement("p");
         statistique.textContent = film.statistique;
 
-        carteFilm.appendChild(titre);
+        carteFilm.appendChild(title);
         carteFilm.appendChild(statistique);
-
         listeFilms.appendChild(carteFilm);
     })
 }
