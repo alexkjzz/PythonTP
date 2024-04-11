1254aba389617728d9925444577c79bdf546b796
lucien-wrq <luluwerq@gmail.com>
2024-04-11 09:24:06 +0200

    bdd

--- v2.0/db.py
+++ v2.0/db.py
@@ -4,11 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 # Configuration de la base de données
 DB_CONFIG = {
-    'HOST': 'mysql-serveurlucien.alwaysdata.net',
-    'USER': '340488',
-    'PASSWORD': 'racinepython2024',
-    'DB_NAME': 'serveurlucien_pythonbdd',
-
+    'DATABASE': '../BDD/python_bdd.bd'
 }
 
 # Initialisation de l'extension SQLAlchemy
@@ -16,6 +12,7 @@ db = SQLAlchemy()
 
 # Configuration de l'application Flask pour la base de données
 def init_app(app):
-    app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql://{DB_CONFIG['USER']}:{DB_CONFIG['PASSWORD']}@{DB_CONFIG['HOST']}/{DB_CONFIG['DB_NAME']}"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_CONFIG['DATABASE']}"
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
+
